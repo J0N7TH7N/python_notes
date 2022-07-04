b822526cd0e330cf812909e526017172a79181e3
J0N7TH7N <82683429+J0N7TH7N@users.noreply.github.com>
2022-07-04 12:12:55 -0500

    Create python_notes.py

---
+++ python_notes.py
@@ -0,0 +1,29 @@
+len gets number of characters from string
+
+parrot = "Norwegian Blue"
+
+print len(parrot)
+#outcome = 14
+
+_____
+
+lower makes letters in string all lowercase
+parrot = "Norwegian Blue"
+
+parrot2 = parrot.lower()
+
+print(parrot2)
+#outcome = norwegian blue
+
+-------------
+upper makes letters in string uppercase
+
+parrot = "norwegian blue"
+
+parrot2 = parrot.upper()
+
+print(parrot2)
+
+-----------------------------------
++ function adds together
+print("hi " + "friend")
