4c76a33f3259c211b82b6fb100a954a83b5b3ee1
J0N7TH7N <82683429+J0N7TH7N@users.noreply.github.com>
2022-07-05 11:05:45 -0500

    Update python_notes.py

--- python_notes.py
+++ python_notes.py
@@ -27,3 +27,21 @@ print(parrot2)
 -----------------------------------
 + function adds together
 print("hi " + "friend")
+--------------------------
+from datetime import datetime
+now = datetime.now()
+
+print("%s/%s/%s" % (now.month, now.day, now.year)) #the %s gets the string and replaces it
+-------------------------------------
+from datetime import datetime
+
+now = datetime.now()
+
+current_year = now.year
+current_month = now.month
+current_day = now.day
+
+print("The year is " + str(now.year))
+print("The month is " + str(now.month))
+print("Today is the "+ str(now.day) + "th") #prints our date
+------------------------
